66136f7de527dc3f48cea3c6dc19559451ae7865
Porter Smith <flowbish@gmail.com>
2018-12-31 15:15:29 -0800

    Push opening the background/fonts up a layer.
    This will make it a little more obvious what assets it's loading.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use image::{Pixel, Rgba, RgbaImage};
-use rusttype::Font;
+use std::path::Path;
+use image::{DynamicImage, Pixel, Rgba, RgbaImage};
 
 mod color;
 mod lettering;
@@ -9,12 +9,6 @@ use crate::lettering::{Letter, Lettering};
 
 const ROW_HEIGHT: f32 = 39.6 / 2.0;
 const HEIGHT: f32 = ROW_HEIGHT * 5.0;
-
-fn open_font(path: &str) -> rusttype::Font {
-    let font_bytes = std::fs::read(path).expect("Error reading font file");
-    let collection = rusttype::FontCollection::from_bytes(font_bytes).unwrap();
-    collection.into_font().unwrap()
-}
 
 fn apply2_alpha<F>(bg: &mut Rgba<u8>, fg: &Rgba<u8>, mut f: F)
 where
@@ -55,14 +49,11 @@ fn new_image_tiled(image: &RgbaImage, width: u32, height: u32) -> RgbaImage {
     })
 }
 
-fn open_image_tiled(path: &str, tiles_x: u32, tiles_y: u32) -> RgbaImage {
-    let image = image::open(path).unwrap().to_rgba();
-    print_time("opened image");
+fn tile_image(image: &DynamicImage, tiles_x: u32, tiles_y: u32) -> RgbaImage {
+    let image = image.to_rgba();
     let (width, height) = (image.width() / 2, image.height() / 2);
     let image = image::imageops::resize(&image, width, height, image::FilterType::Lanczos3);
-    print_time("resized image");
     let image = new_image_tiled(&image, tiles_x * image.width(), tiles_y * image.height());
-    print_time("tiled image");
     image
 }
 
@@ -93,7 +84,7 @@ impl<'a> LetteredBoard<'a> {
     }
 
     pub fn render(&self) -> RgbaImage {
-        let mut image = self.board.image.clone();
+        let mut image = self.board.background.image.clone();
         for (n, line) in self.lettering.lines().enumerate() {
             for (glyph, color) in line.letters() {
                 LetteredBoard::render_glyph(
@@ -110,37 +101,45 @@ impl<'a> LetteredBoard<'a> {
     }
 }
 
+pub struct Background {
+    image: RgbaImage,
+}
+
+impl Background {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Background, image::ImageError> {
+        let dyn_image = image::open(path)?;
+        let image = tile_image(&dyn_image, 2, 2);
+        Ok(Background { image })
+    }
+}
+
+pub struct Font {
+    inner: rusttype::Font<'static>,
+}
+
+impl Font {
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Font, std::io::Error> {
+        let font_bytes = std::fs::read(path)?;
+        let collection = rusttype::FontCollection::from_bytes(font_bytes)?;
+        Ok(Font { inner: collection.into_font()? })
+    }
+}
+
 /// Represents the configuration for generating a board. This includes the background iamges and
 /// fonts required to render the final image.
 pub struct Board {
-    image: RgbaImage,
-    font: Font<'static>,
-    shade: Font<'static>,
-}
-
-fn print_time(prefix: &str) {
-    use std::time::SystemTime;
-    let duration = SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap();
-    println!(
-        "{}: {}.{}",
-        prefix,
-        duration.as_secs(),
-        duration.subsec_millis()
-    );
+    background: Background,
+    font: Font,
+    shade: Font,
 }
 
 impl Board {
-    pub fn new() -> Self {
-        let image = open_image_tiled("gray.jpg", 2, 2);
-        let font = open_font("font.ttf");
-        let shade = open_font("shade.ttf");
-        Board { image, font, shade }
+    pub fn new(background: Background, font: Font, shade: Font) -> Self {
+        Board { background, font, shade }
     }
 
     pub fn write_phrase<'a>(&'a self, phrase: &str) -> LetteredBoard<'a> {
-        let mut lettering = Lettering::new(&self.font, &self.shade);
+        let mut lettering = Lettering::new(&self.font.inner, &self.shade.inner);
         for letter in phrase.chars() {
             lettering.put_character(letter);
         }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use felt::Board;
+use felt::{Font, Background, Board};
 
 // Create a board that loads in the background and font.
 // Interact with the board to write a phrase and get a LetteredBoard.
@@ -7,7 +7,12 @@ use felt::Board;
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
     let phrase = &args[1];
-    let board = Board::new();
+
+    let background = Background::open("gray.jpg").expect("Unable to open background");
+    let font = Font::open("font.ttf").expect("Unable to open font");
+    let shade = Font::open("shade.ttf").expect("Unable to open shade");
+    let board = Board::new(background, font, shade);
+
     let board = board.write_phrase(phrase);
     let image = board.render();
     image.save("output.png").expect("Error saving image");
